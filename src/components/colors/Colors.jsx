edff3e6c1723bfdc7a2d92fd99882fe40f67884e
Bernabe <bernatattoo@gmail.com>
2023-06-29 01:30:56 -0300

    Add colors game, no Styles

--- src/components/colors/Colors.jsx
+++ src/components/colors/Colors.jsx
@@ -1,41 +1,58 @@
 import "./colors.styles.css";
-import { useState, useEffect } from "react";
-
-const COLORS = [
-  {
-    name: "Rojo",
-    color: "#f00",
-  },
-  {
-    name: "Azul",
-    color: "#1900ff",
-  },
-  {
-    name: "verde",
-    color: "#01a009",
-  },
-  {
-    name: "Naranja",
-    color: "#ff7b00",
-  },
-];
+import { useState, useEffect, useMemo } from "react";
+import { COLORS } from "../../data/colors";
+import { Link } from "react-router-dom";
 
 const Colors = () => {
   const [estado, setEstado] = useState("initial");
   const [time, setTime] = useState(0);
   const [score, setScore] = useState(0);
-  const [color, setColor] = useState(null);
-  const [wrongColor, setWrongColor] = useState(null);
+  const [gameColors, setGameColors] = useState([]);
+  // asigna el color correcto a un boton
+  const correctColor = useMemo(
+    () => gameColors.find((color) => color.correct),
+    [gameColors]
+  );
 
-  const hadlePlay = () => {
+  const handlePlay = () => {
     setEstado("playing");
     setTime(0);
     setScore(0);
-    const [color, wrongColor] = COLORS.slice().sort(() => Math.random() - 0.5);
-    setColor(color);
-    setWrongColor(wrongColor); //Escoje un color random para los botones y la palabra
+    const [correctColor, wrongColor] = COLORS.slice().sort(
+      () => Math.random() - 0.5
+    );
+    // mezcla los colores y coloca uno como correcto
+    setGameColors(
+      [{ ...correctColor, correct: true }, wrongColor].sort(
+        () => Math.random() - 0.5
+      )
+    );
   };
 
+  function handleColor(selectedColor) {
+    if (selectedColor.correct) {
+      setScore((score) => score + 1);
+      if (score === 9) {
+        alert("El juego ha terminado!");
+        setEstado("finish");
+      } else {
+        const [correctColor, wrongColor] = COLORS.slice().sort(
+          () => Math.random() - 0.5
+        );
+        setGameColors(
+          [{ ...correctColor, correct: true }, wrongColor].sort(
+            () => Math.random() - 0.5
+          )
+        );
+      }
+    } else {
+      setScore((score) => score - 1);
+    }
+  }
+
+  /* The `useEffect` hook is used to perform side effects in a React component. In this case, the
+  `useEffect` hook is used to start and stop a timer based on the value of the `estado` state
+  variable. */
   useEffect(() => {
     let interval;
     if (estado === "playing") {
@@ -56,23 +73,32 @@ const Colors = () => {
       </header>
       {estado === "playing" && (
         <span>
-          <h1 style={{ color: wrongColor.color }}>{color?.name}</h1>
+          <h1 style={{ color: gameColors[0].color }}>{correctColor.name}</h1>
         </span>
       )}
-      {estado === "initial" && <button onClick={hadlePlay}>Jugar</button>}
-      {estado === "finish" && (
-        <button onClick={() => setEstado("initial")}>Reiniciar</button>
-      )}
-      {estado === "playing" && color && (
+      {estado === "initial" && <button onClick={handlePlay}>Jugar</button>}
+      {estado === "finish" && <button onClick={handlePlay}>Reiniciar</button>}
+      {estado === "playing" && (
         <div className="btn-container">
           <button
-            style={{ width: 92, height: 92, backgroundColor: color.color }}
+            onClick={() => handleColor(gameColors[0])}
+            style={{
+              width: 92,
+              height: 92,
+              backgroundColor: gameColors[0].color,
+            }}
           ></button>
           <button
-            style={{ width: 92, height: 92, backgroundColor: wrongColor.color }}
+            onClick={() => handleColor(gameColors[1])}
+            style={{
+              width: 92,
+              height: 92,
+              backgroundColor: gameColors[1].color,
+            }}
           ></button>
         </div>
       )}
+      <Link to="/">Home</Link>
     </main>
   );
 };
---
+++ src/data/colors.js
@@ -0,0 +1,32 @@
+export const COLORS = [
+    {
+        name: "Rojo",
+        color: "#f00",
+        correct: false
+    },
+    {
+        name: "Azul",
+        color: "#1900ff",
+        correct: false
+    },
+    {
+        name: "verde",
+        color: "#01a009",
+        correct: false
+    },
+    {
+        name: "Naranja",
+        color: "#ff7b00",
+        correct: false
+    },
+    {
+        name: "Violeta",
+        color: "#78288c",
+        correct: false
+    },
+    {
+        name: "Celeste",
+        color: "#28bac1",
+        correct: false
+    },
+];
